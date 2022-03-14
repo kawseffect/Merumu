2e3928a1af01101e213b1e29b873c3d9a3435c38
Mwoon <kensei.dgok@gmail.com>
2022-03-14 14:12:05 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,61 @@
+// Warnable 2.0.0 - Database (node-json-db)
+var options = {
+    // db_name: The name of the file that will save in the folder to hold all user warning data.
+    db_name: "merumu"
+}
+
+const nodejsondb = require("node-json-db").JsonDB;
+
+module.exports = class db {
+    constructor() {
+        this.db = new nodejsondb(options.db_name, true, true);
+    }
+
+    addWarning(guild, user, d, reason, issuer) { // Returns: Number (The users new total warning point value)
+        return new Promise((resolve, reject) => {
+            try {
+                this.db.push(`/guilds/${guild}/users/${user}/warnings[]`, { d, reason, issuer }, true);
+                this.db.push(`/guilds/${guild}/last`, user);
+                // Return data
+                resolve(this.db.getData(`/guilds/${guild}/users/${user}/warnings`).reduce((prev, val) => prev + val.points, 0))
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    getWarnings(guild, user, pos) { // Returns: Depends: Check below...
+        return new Promise((resolve, reject) => {
+            try {
+                if (pos) { // If requesting single warning.
+                    pos = (pos - 1) * -1;
+                    // Return warning object
+                    resolve(this.db.getData(`/guilds/${guild}/users/${user}/warnings[${pos}]`));
+                }
+                else { // If requesting all warnings.
+                    // Return array of warning objects. newest to oldest.
+                    resolve(this.db.getData(`/guilds/${guild}/users/${user}/warnings`).reverse());
+                }
+            }
+            catch (err) { // If no warnings: Returns empty array.
+                resolve([]);
+            }
+        });
+    }
+
+    removeWarning(guild, user, pos) { // Returns: User ID (no clue why, could be anything i think lol...)
+        return new Promise((resolve, reject) => {
+            try {
+                pos = (!user) ? -1 : (pos - 1) * -1;
+                user = (!user) ? this.db.getData(`/guilds/${guild}/last`) : user; 
+                if (!user) this.db.delete(`/guilds/${guild}/last`);
+                this.db.delete(`/guilds/${guild}/users/${user}/warnings[${pos}]`);
+                resolve(user);
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
