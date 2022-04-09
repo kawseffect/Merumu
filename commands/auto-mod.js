2a379ef0b2b7b3553f5adc1d23da722b5661a9c6
Mwoon <kensei.dgok@gmail.com>
2022-04-09 13:29:21 -0400

    Update auto-mod.js
    made it prettier

--- commands/auto-mod.js
+++ commands/auto-mod.js
@@ -9,60 +9,79 @@ const onEmbed = new MessageEmbed()
   .setColor(0x57cf23)
   .setDescription(
     `<:MeruYes:952435870491893810> | ${bold(
-      italic('Enabled the Auto-Moderation feature!')
-    )}\n\n${bold('What is the auto-mod feature?')}\n${italic(
-      "The auto-mod feature is a command for moderators so they don't have to moderate everything and take a little weight off their backs!"
+      italic('Enabled the Anti-Server-Raiding feature!')
+    )}\n\n${bold('What is the anti-raid feature?')}\n${italic(
+      'The anti-raid features are custom commands built to help prevent raids from taking place and stop them right on their tracks, from closing the entire server so no one can speak to join-bans to instantly ban users on join!'
     )}\n\n${bold('Want to turn this feature off?')}\n${codeBlock(
       'txt',
-      '/auto-mod set mode:off'
+      '/antiraid set mode:off'
     )}`
   );
 const offEmbed = new MessageEmbed()
   .setColor(0xff3636)
   .setDescription(
     `<:MeruNo:952435833649106964> | ${bold(
-      italic('Disabled the Auto-Moderation feature!')
-    )}\n\n${bold('What is the auto-mod feature?')}\n${italic(
-      "The auto-mod feature is a command for moderators so they don't have to moderate everything and take a little weight off their backs!"
+      italic('Disabled the Anti-Server-Raiding feature!')
+    )}\n\n${bold('What is the anti-raid feature?')}\n${italic(
+      'The anti-raid features are custom commands built to help prevent raids from taking place and stop them right on their tracks, from closing the entire server so no one can speak to join-bans to instantly ban users on join!'
     )}\n\n${bold('Want to turn this feature on?')}\n${codeBlock(
       'txt',
-      '/auto-mod set mode:on'
+      '/antiraid set mode:off'
     )}`
   );
-const offStr = 'Off | <:MeruNo:952435833649106964>';
+const offStr = 'Deactivated | <:MeruNo:952435833649106964>';
 
 export default {
   data: {
-    name: 'auto-mod',
-    description: 'Configures the auto-mod features.',
+    name: 'antiraid',
+    description: 'Configures the anti-raid feature.',
     options: [
       {
         name: 'set',
-        description: 'Sets the auto-mod mode.',
-        type: 'SUB_COMMAND',
+        description: 'Sets the anti-raid feature configurations.',
+        type: 'SUB_COMMAND_GROUP',
         options: [
           {
             name: 'mode',
-            description: 'The auto-mod mode.',
-            type: 'STRING',
-            choices: [
+            description: 'Sets the anti-raid mode.',
+            type: 'SUB_COMMAND',
+            options: [
               {
-                name: 'on',
-                value: 'on'
-              },
-              {
-                name: 'off',
-
-                value: 'off'
+                name: 'mode',
+                description: 'The anti-raid feature mode.',
+                type: 'STRING',
+                choices: [
+                  {
+                    name: 'on',
+                    value: 'on'
+                  },
+                  {
+                    name: 'off',
+                    value: 'off'
+                  }
+                ],
+                required: true
               }
-            ],
-            required: true
+            ]
+          },
+          {
+            name: 'role',
+            description: 'Sets the anti-raid role.',
+            type: 'SUB_COMMAND',
+            options: [
+              {
+                name: 'role',
+                description: 'The anti-raid role.',
+                type: 'ROLE',
+                required: true
+              }
+            ]
           }
         ]
       },
       {
         name: 'show',
-        description: 'Shows the current auto-mod feature mode.',
+        description: 'Shows the currnt antiraid feature mode.',
         type: 'SUB_COMMAND'
       }
     ]
@@ -72,65 +91,102 @@ export default {
    * @param {import('discord.js').CommandInteraction} interaction
    */
   async execute(client, interaction) {
+    const subcommandGroup = interaction.options.getSubcommandGroup(false);
     const subcommand = interaction.options.getSubcommand();
+    
 
-    if (subcommand === 'set') {
-      if (
-        !interaction.member.permissions.any([ADMINISTRATOR, MANAGE_MESSAGES])
-      ) {
-        await interaction.reply({
-          content: 'You need to be an admin or a mod to use this command.',
-          ephemeral: true
-        });
+    if (subcommandGroup === 'set') {
+      if (subcommand === 'mode') {
 
-        return;
+        if (
+          !interaction.member.permissions.any([ADMINISTRATOR, MANAGE_MESSAGES])
+        ) {
+          await interaction.reply({
+            content: 'You need to be an admin or a mod to use this command.',
+            ephemeral: true
+          });
+  
+          return;
+        }
+        
+
+        const enable = interaction.options.getString('mode') === 'on';
+
+        await client.db.updateGuild(
+          interaction.guild.id,
+          { id: interaction.guild.id },
+          { $set: { antiRaid: 'Enabled | <:MeruYes:952435870491893810> ' } }
+        );
+
+        await interaction.reply({ embeds: [enable ? onEmbed : offEmbed] });
+      } else if (subcommand === 'role') {
+
+        if (
+          !interaction.member.permissions.any([ADMINISTRATOR, MANAGE_MESSAGES])
+        ) {
+          await interaction.reply({
+            content: 'You need to be an admin or a mod to use this command.',
+            ephemeral: true
+          });
+  
+          return;
+        }
+
+        const role = interaction.options.getRole('role');
+
+        await client.db.updateGuild(
+          interaction.guild.id,
+          { id: interaction.guild.id },
+          { $set: { lockdownRole: role.id } }
+        );
+
+        const embed = new MessageEmbed()
+          .setColor(0x57cf23)
+          .setDescription(
+            `<:MeruYes:952435870491893810> | The lockdown role has successfully been set to ${role.toString()}`
+          );
+
+        await interaction.reply({ embeds: [embed] });
       }
-
-      const enable = interaction.options.getString('mode') === 'on';
-
-      await client.db.updateGuild(
-        interaction.guild.id,
-        { id: interaction.guild.id },
-        { $set: { automod: enable } }
-      );
-
-      await interaction.reply({ embeds: [enable ? onEmbed : offEmbed] });
-    } else {
-      const { automod, modSpam, modInvite, modBadWord, modLogging } =
+    } else if (subcommand === 'show') {
+      const { antiRaid, raidLock, raidJoin, raidSlow } =
         await client.db.updateGuild(interaction.guild.id);
 
       const embed = new MessageEmbed()
         .setColor('RANDOM')
         .setDescription(
           `${quote(
-            bold(
-              italic(
-                `The Auto-Moderation feature is currently set to ${
-                  automod ?? offStr
-                }`
-              )
-            )
-          )}\n\n${quote(bold(italic('The auto-mod features:')))}\n${italic(
-            'badwords, message-spam and invite-links'
-          )}\n\n${quote(bold(italic('Feature statuses:')))}\n${bold(
-            `Message spam filter is currently set to ${modSpam ?? offStr}`
+            bold(italic(`The Anti-Raid is currently ${antiRaid ?? offStr}`))
+          )}\n\n${quote(bold(italic('The Anti-Raid features:')))}\n${italic(
+            'join-ban, slowmode, and lock/unlock'
+          )}\n\n${quote(
+            bold(italic('The Anti-Raid feature statuses:'))
           )}\n${bold(
-            `Moderation logging is currently set to ${modLogging ?? offStr}`
+            `Lockdown mode is currently ${raidLock ?? offStr}`
           )}\n${bold(
-            `Invite links filter is currently set to ${modInvite ?? offStr}`
+            `Join-ban mode is currently ${raidJoin ?? offStr}`
           )}\n${bold(
-            `Badwords filter is currently set to ${modBadWord ?? offStr}`
-          )}\n\n${italic(
-            'If you want to enable any of the features just do'
+            `Slowmode feature is currently ${raidSlow ?? offStr}`
+          )}\n${italic(
+            'If you want to enable any of these features just do'
           )}\n${codeBlock(
             'txt',
             '/<feature> set mode:on/off'
           )}\nTo switch between modes!\n${italic(
-            'Want to enable automod?'
-          )} Use\n${codeBlock(
+            'Want to enable the anti-raid feature?'
+          )}\nUse\n${codeBlock(
             'txt',
-            '/auto-mod set mode:on/off'
-          )}\nTo toggle it on or off!`
+            '/anti-raid set mode mode:on/off'
+          )}\nTo toggle it on or off!\n${quote(
+            bold(italic('New feature!'))
+          )}\nUse\n${codeBlock(
+            'txt',
+            '/anti-raid set role role:@role'
+          )}\nTo set the default role for your members!\n\n${quote(
+            bold(italic('What is this for?'))
+          )}\n${italic(
+            'This feature is so that if you have an auto-role bot enabled whatever your default member role is (e.g. Members, New Friends, etc etc), this role will be locked down during a raid!'
+          )}`
         );
 
       await interaction.reply({ embeds: [embed] });
