87c189fc8c243e738135fae6cbfdcffd904884b3
Moo n1 saN <kensei.dgok@gmail.com>
2022-04-16 21:12:11 -0400

    Add files via upload

---
+++ commands/automod/badwords.js
@@ -0,0 +1,144 @@
+import { Permissions, MessageEmbed, Formatters } from 'discord.js';
+
+const {
+  FLAGS: { ADMINISTRATOR, MANAGE_MESSAGES }
+} = Permissions;
+const { bold, italic, codeBlock } = Formatters;
+
+
+  export default {
+    data: {
+      name: 'badwords',
+      description: 'Configures the badwords feature.',
+      options: [
+        {
+          name: 'set',
+          description: 'Sets the badwords feature configurations.',
+          type: 'SUB_COMMAND_GROUP',
+          options: [
+            {
+              name: 'mode',
+              description: 'Sets the badwords mode.',
+              type: 'SUB_COMMAND',
+              options: [
+                {
+                  name: 'mode',
+                  description: 'The badwords feature mode.',
+                  type: 'STRING',
+                  choices: [
+                    {
+                      name: 'on',
+                      value: 'on'
+                    },
+                    {
+                      name: 'off',
+                      value: 'off'
+                    }
+                  ],
+                  required: true
+                }
+              ]
+            }
+      ]
+    }
+  ]
+    },
+    /**
+     * @param {import('discord.js').Client} client
+     * @param {import('discord.js').CommandInteraction} interaction
+     */
+    async execute(client, interaction) {
+      const onEmbed = new MessageEmbed()
+  .setColor(0x57cf23)
+  .setDescription(
+    `${client.emotes} | ${bold(
+      italic('Enabled the Badwords feature!')
+    )}\n\n${bold('What is the badwords feature?')}\n${italic(
+      'The badwords feature is part of the auto-mod commands and category, this command enables the bot to read messages and delete messages containing blacklisted words'
+    )}\n\n${bold('Want to turn this feature off?')}\n${codeBlock(
+      'txt',
+      '/badwords set mode:off'
+    )}`
+  );
+const offEmbed = new MessageEmbed()
+  .setColor(0xff3636)
+  .setDescription(
+    `${client.emotes} | ${bold(
+      italic('Disabled the Badwords feature!')
+    )}\n\n${bold('What is the badwords feature?')}\n${italic(
+        'The badwords feature is part of the auto-mod commands and category, this command enables the bot to read messages and delete messages containing blacklisted words'
+    )}\n\n${bold('Want to turn this feature on?')}\n${codeBlock(
+      'txt',
+      '/badwords set mode:on'
+    )}`
+  );
+
+      const subcommandGroup = interaction.options.getSubcommandGroup(false);
+      const subcommand = interaction.options.getSubcommand();
+  
+      if (subcommandGroup === 'set') {
+        if (subcommand === 'mode') {
+          const { automod } = await client.db.updateGuild(interaction.guild.id);
+
+        if (!automod) {
+          const embed = new MessageEmbed()
+          .setColor(0xff3636)
+          .setDescription(
+            client.emotes + '| Please enable the automod feature first before using this command!'
+          );
+  
+          await interaction.reply({ embeds: [embed] });
+  
+          return;
+            }
+          if (
+            !interaction.member.permissions.any([ADMINISTRATOR, MANAGE_MESSAGES])
+          ) {
+            await interaction.reply({
+              content: 'You need to be an admin or a mod to use this command.',
+              ephemeral: true
+            });
+  
+            return;
+          }
+  
+        if (!interaction.member.permissions.any([ADMINISTRATOR, MANAGE_MESSAGES])) {
+          await interaction.reply({
+            content: 'You need to be an admin or a mod to use this command.',
+            ephemeral: true
+          });
+    
+          return;
+        }
+        const enable = interaction.options.getString('mode')
+
+        if(enable === 'on') {
+  
+          await interaction.reply({ embeds: [ onEmbed] });
+    
+    
+          await client.db.updateGuild(
+            interaction.guild.id,
+            { id: interaction.guild.id },
+            { $set: { modBadWord: 'On' } }
+          );
+
+        }else if (enable === 'off') {
+  
+          await interaction.reply({ embeds: [ offEmbed] });
+  
+          await client.db.updateGuild(
+            interaction.guild.id,
+            { id: interaction.guild.id },
+            { $set: { modBadWord: 'Off' } }
+          );
+    
+          
+  
+        }
+  
+        } 
+      }
+        
+      }
+    }
